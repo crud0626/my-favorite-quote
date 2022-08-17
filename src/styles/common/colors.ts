701d6acd5ff99d7409b2a1bd888f4ed3c4261253
crud0626 <sisofiy6048@gmail.com>
2022-08-17 16:34:36 +0900

    Styles: Add css prop variables
    * Add colors file
    
    * Add sizes file

---
+++ src/styles/common/colors.ts
@@ -0,0 +1,12 @@
+// Common
+export const MAIN_WHITE = "#F9F9F9";
+export const MAIN_BLACK = "#1E1F21";
+export const SUB_BLACK = "#292A2E";
+export const LIGHT_GRAY = "#D3D3D3";
+export const CONTAINER_ALPHA = "rgba(0, 0, 0, 0.3)";
+export const ALERT_ALPHA = "rgba(0, 0, 0, 0.5)";
+
+// Button
+export const BUTTON_RED = "#DC0000";
+export const BUTTON_HOVER = "rgba(0, 0, 0, 0.05)";
+export const BUTTON_ACTIVE = "rgba(0, 0, 0, 0.1)";
---
+++ src/styles/common/sizes.ts
@@ -0,0 +1,27 @@
+export const BODY_MIN_WIDTH = "320px";
+export const HEADER_HEIGHT = "49px";
+export const FOOTER_HEIGHT = "49px";
+export const SECTION_HEIGHT = `100vh - ${HEADER_HEIGHT} - ${FOOTER_HEIGHT}`;
+
+// Icon
+export const DEFAULT_ICON_SIZE = "24px";
+export const LARGE_ICON_SIZE = "32px";
+export const CHEVRON_ICON_SIZE = "64px";
+
+// Viewport
+export const TABLET_VIEWPORT_SIZE = "768px";
+export const MOBILE_VIEWPORT_SIZE = "320px";
+
+// Card
+export const CARD_WIDTH = "500px";
+export const CARD_HEIGHT = "700px";
+export const CARD_TITLE_FONT_SIZE = "1.3rem";
+export const CARD_AUTHOR_FONT_SIZE = "1rem";
+
+// Font
+export const HEAD_SIZE = "2rem";
+export const NAV_AUTHOR_FONT_SIZE = "0.8rem";
+
+// Margin & Padding
+export const DEFAULT_SPACE_SIZE = "28px";
+export const SMALL_SPACE_SIZE = "8px";
