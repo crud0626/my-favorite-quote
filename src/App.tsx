cd687260b8b7d67f938804c9145675416df71685
crud0626 <sisofiy6048@gmail.com>
2022-08-31 20:41:08 +0900

    Feat: Add quoteHistory state in App
    * Add logic to requestData function to change the value of quoteHistory

--- src/App.tsx
+++ src/App.tsx
@@ -10,20 +10,29 @@ const quotesAPI = new QuotesAPI();
 const App = () => {
     const [isNavOpen, setIsNavOpen] = useState<boolean>(false);
     const [quoteData, setQuoteData] = useState<QuoteData | null>(null);
+    const [quoteHistory, setQuoteHistory] = useState<QuoteData[]>([]);
 
     const handleNav = () => setIsNavOpen(state => !state);
 
-    useEffect(() => {
-        (async function() {
-            try {
-                const resData = await quotesAPI.getQuotesData();
-                if (resData) {
-                    setQuoteData(resData);
-                }
-            } catch (error) {
-                alert("데이터를 요청 하던 도중 에러가 발생했습니다.");
+    const requestData = async (id?: string): Promise<any> => {
+        try {
+            const resData = await quotesAPI.getQuotesData(id);
+            if (resData) {
+                const newHistoryList = [...quoteHistory];
+                newHistoryList.unshift(resData);
+
+                setQuoteData(resData);
+                setQuoteHistory(newHistoryList);
+                return true;
             }
-        })();
+        } catch (error) {
+            alert("데이터를 요청 하던 도중 에러가 발생했습니다.");
+            return false;
+        }
+    }
+
+    useEffect(() => {
+        requestData();
     }, []);
 
     return (
@@ -36,6 +45,8 @@ const App = () => {
                 <Main 
                     isNavOpen={isNavOpen} 
                     quoteData={quoteData}
+                    quoteHistory={quoteHistory}
+                    requestData={requestData}
                 />
                 <Footer />
         </>
