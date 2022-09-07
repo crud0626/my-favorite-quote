8526515215034c74ad5ea4f1c791a52ee0b8306c
crud0626 <sisofiy6048@gmail.com>
2022-09-07 18:13:03 +0900

    Feat: Add logic to functions in sessionStorage
    Add logic associated with the favorite feature to the functions

--- src/utils/sessionStorage.ts
+++ src/utils/sessionStorage.ts
@@ -1,18 +1,36 @@
-import { QuoteData } from './../services/quotesApi';
+import { QuoteData } from './../App';
+import { ResponseQuote } from '../services/quotesApi';
 
 const STORAGE = window.sessionStorage;
 
 export const getStorageData = (): QuoteData[] | null => {
-    const data = STORAGE.getItem("mfq-history-list");
+    const savedData = STORAGE.getItem("mfq-history-list");
 
-    if (data) {
-        const dataArr = JSON.parse(data);
-        return dataArr.length > 0 ? dataArr : null;
+    if (savedData) {
+        const savedArr = JSON.parse(savedData);
+        const dataWithFavorite = savedArr.map((item: ResponseQuote): QuoteData => {
+            const newItem: QuoteData = { ...item, favorite: false };
+            return newItem;
+        });
+
+        return dataWithFavorite;
     }
     
     return null;
 }
 
 export const saveStorageData = (data: QuoteData[]) => {
-    STORAGE.setItem("mfq-history-list", JSON.stringify(data));
+    let saveData: ResponseQuote[] = data.map((item: QuoteData) => {
+        const newItem: any = {};
+
+        for(let [key, value] of Object.entries(item)) {
+            if(key !== "favorite") {
+                newItem[key] = value;
+            }
+        }
+
+        return newItem;
+    });
+
+    STORAGE.setItem("mfq-history-list", JSON.stringify(saveData));
 }
