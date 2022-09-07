3c196f4f95ea98a431427d8995a2418b1fc43036
crud0626 <sisofiy6048@gmail.com>
2022-09-07 20:24:19 +0900

    Chore: Add config for applying firebase DB

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -7,7 +7,8 @@ const firebaseConfig = {
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    databaseURL: process.env.FIREBASE_DATABASE_URL
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
