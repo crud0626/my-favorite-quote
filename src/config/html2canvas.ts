9cdefae88dfec9d00f35811ee28f8798b3bd14ee
crud0626 <sisofiy6048@gmail.com>
2022-10-13 10:06:57 +0900

    Fix: Fix a bug related to image opacity
    Modify background color of html2canvas configuration
    
    Resolves: #2

--- src/config/html2canvas.ts
+++ src/config/html2canvas.ts
@@ -1,8 +1,10 @@
+import * as colors from '~/styles/common/colors';
+
 export const HTML2CANVAS_CONFIG = {
     ignoreElements: (el: Element): boolean => (
         el instanceof HTMLDivElement 
         ? el.className.includes("inner_btn_wrapper")
         : false
     ),
-    backgroundColor: null
+    backgroundColor: colors.MAIN_BLACK
 };
