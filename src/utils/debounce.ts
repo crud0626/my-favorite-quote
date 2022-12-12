a4985aef5c17aa0d7c60dfc5eaf9d89a413d4f6b
crud0626 <sisofiy6048@gmail.com>
2022-12-12 20:32:36 +0900

    Refactor: Modify debounce funtion
    Change the type of timer in debounce

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -2,7 +2,8 @@ export function debounce<F extends (...args: Parameters<F>) => ReturnType<F>>(
     callbackFn: F,
     timeout: number
 ): (...args: Parameters<F>) => void {
-    let timer: NodeJS.Timeout;
+    let timer: ReturnType<typeof setTimeout>;
+    
     return (...args: Parameters<F>): void => {
         clearTimeout(timer);
         timer = setTimeout(() => callbackFn(...args), timeout);
