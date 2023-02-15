9b02899823dc84348b211eb1b9f5438d36650ed6
crud0626 <sisofiy6048@gmail.com>
2023-02-15 11:53:52 +0900

    Feat: Add direction in card event

--- src/components/Main/ChevronBox/ChevronBox.tsx
+++ src/components/Main/ChevronBox/ChevronBox.tsx
@@ -4,28 +4,30 @@ import { useCardStore } from '~/stores/useCardStore';
 import * as colors from '~/styles/common/colors';
 import { ChevronButton, ChevronWrapper } from './ChevronBox.styles';
 import { debounce } from '~/utils/debounce';
+import { ChevronEventType } from '~/types/user.type';
 import { LeftChevron, RightChevron } from '~/assets';
 
 const ChevronBox = () => {
     const { changeDisplayQuote} = useCardStore();
     const { requestQuote } = useQuotesStore();
 
-    const fetchNewQuote = () => {
+    const fetchNewQuote = useCallback((direction: ChevronEventType) => {
         requestQuote()
         .then(newQuote => {
             if (!newQuote) return;
 
-            changeDisplayQuote(newQuote);
-        })
-    }
-    const onClick = useCallback(debounce(fetchNewQuote, 300, true), []);
+            changeDisplayQuote(newQuote, direction);
+        });
+    }, []);
+    
+    const onClick = useCallback(debounce(fetchNewQuote, 300, true), [fetchNewQuote]);
 
     return (
         <ChevronWrapper>
-            <ChevronButton direction={"prev"} data-role="prev" onClick={() => onClick()}>
+            <ChevronButton direction={"prev"} onClick={() => onClick("prev")}>
                 <LeftChevron fill={colors.MAIN_WHITE} />
             </ChevronButton>
-            <ChevronButton direction={"next"} data-role="next" onClick={() => onClick()}>
+            <ChevronButton direction={"next"} onClick={() => onClick("next")}>
                 <RightChevron fill={colors.MAIN_WHITE} />
             </ChevronButton>
         </ChevronWrapper>
