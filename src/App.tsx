9becfa3bca7e46ef763b85f9591aff648f62620c
crud0626 <sisofiy6048@gmail.com>
2023-02-21 16:06:25 +0900

    Fix: Fix a bug related to card rotation (#6)

--- src/App.tsx
+++ src/App.tsx
@@ -10,27 +10,20 @@ import GlobalStyle from '~/styles/common/GlobalStyle';
 import { getStorageData } from '~/utils/sessionStorage';
 
 const App = () => {
-    const { replaceQuotes, getUserQuotes, requestQuote } = useQuotesStore();
+    const { replaceQuotes, getUserQuotes } = useQuotesStore();
     const { changeDisplayQuote } = useCardStore();
-    const { checkLoggedIn } = useUserStore();    
+    const { addUserStateListener } = useUserStore();    
     
     const setLocalData = () => {
         const savedUserData = getStorageData();
 
         if(savedUserData) {
             const latestHistory = savedUserData.history[0];
-
+            
             replaceQuotes(savedUserData);
             changeDisplayQuote(latestHistory);
             return;
         }
-
-        requestQuote()
-        .then(newQuote => {
-            if (!newQuote) return;
-
-            changeDisplayQuote(newQuote);
-        });
     }
 
     const initData = async (user: User | null) => {
@@ -47,7 +40,7 @@ const App = () => {
     }
 
     useEffect(() => {
-        checkLoggedIn(initData);
+        addUserStateListener(initData);
     }, []);
 
     return (
