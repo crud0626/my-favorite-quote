859290731bad4a78079810ff824a0103b8d4fb84
crud0626 <sisofiy6048@gmail.com>
2023-03-10 17:33:25 +0900

    Chore: Complete configuration related to testing
    * Create babel.config.js file
    
    * Create jest.config.js file
    
    * Modify test command in package.json

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
